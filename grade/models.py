038e283305426578590ac885e1a70723f734cde4
Keijun Kumagai <poittyo091221@gmail.com>
2018-03-02 19:38:15 -0500

    change the place number

--- grade/models.py
+++ grade/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 # Create your models here.
 class Grade(models.Model):
-    basic_seminar          = models.DecimalField(max_digits=2, decimal_places=1,default=0)
-    language_english       = models.DecimalField(max_digits=2, decimal_places=1,default=0)
-    sport                  = models.DecimalField(max_digits=2, decimal_places=1,default=0)
-    human_basic            = models.DecimalField(max_digits=2, decimal_places=1,default=0)
-    science_basic_human    = models.DecimalField(max_digits=2, decimal_places=1,default=0)
-    science_basic_science  = models.DecimalField(max_digits=2, decimal_places=1,default=0)
-    liberal_human          = models.DecimalField(max_digits=2, decimal_places=1,default=0)
-    liberal_science        = models.DecimalField(max_digits=2, decimal_places=1,default=0)
-    liberal_all            = models.DecimalField(max_digits=2, decimal_places=1,default=0)
-    liberal_free           = models.DecimalField(max_digits=2, decimal_places=1,default=0)
-    major                  = models.DecimalField(max_digits=2, decimal_places=1,default=0)
+    basic_seminar          = models.DecimalField(max_digits=3, decimal_places=1,default=0)
+    language_english       = models.DecimalField(max_digits=3, decimal_places=1,default=0)
+    sport                  = models.DecimalField(max_digits=3, decimal_places=1,default=0)
+    human_basic            = models.DecimalField(max_digits=3, decimal_places=1,default=0)
+    science_basic_human    = models.DecimalField(max_digits=3, decimal_places=1,default=0)
+    science_basic_science  = models.DecimalField(max_digits=3, decimal_places=1,default=0)
+    liberal_human          = models.DecimalField(max_digits=3, decimal_places=1,default=0)
+    liberal_science        = models.DecimalField(max_digits=3, decimal_places=1,default=0)
+    liberal_all            = models.DecimalField(max_digits=3, decimal_places=1,default=0)
+    liberal_free           = models.DecimalField(max_digits=3, decimal_places=1,default=0)
+    major                  = models.DecimalField(max_digits=3, decimal_places=1,default=0)
 
     def __str__(self):
         return str(self.pk)
